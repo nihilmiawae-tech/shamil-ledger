e4bb347fd26dc6708877a26f72eb11288d5f5fd3
nihilmiawae-tech <nihilmiawae@gmail.com>
2026-01-30 19:39:26 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/androidIdentifier/MainActivity.kt
+++ android/app/src/main/kotlin/androidIdentifier/MainActivity.kt
@@ -1,5 +1,6 @@
-package {{androidIdentifier}}
+package com.example.shamil_ledger
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
